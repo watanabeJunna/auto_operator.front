bd2eb879bcfedafb4ab8e1f61e18c62106a2f108
watanabeJunna <junna7897@icloud.com>
2019-08-15 16:53:06 +0900

    ref: routing isolation

---
+++ server/route.ts
@@ -0,0 +1,39 @@
+import { Router, Request, Response } from 'express'
+import Cipher from "../libs/Cipher"
+import Account, { IAccount } from "./models/Account"
+
+const router: Router = Router()
+
+router.post('/account/sign_in', async (req: Request, res: Response) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    /**
+     * TODO:
+     * 4. session
+     */
+    if (typeof username !== 'string' || typeof password !== 'string') {
+        res.send({ ok: false })
+        return
+    }
+
+    const encryptedPass: string = Cipher.encrypt(password)
+
+    const user: IAccount | null = await Account.findOne({
+        username: username,
+        password: encryptedPass
+    }, (err: Error) => {
+        if (err) {
+            console.log(err)
+        }
+    })
+
+    if (!user) {
+        res.send({ ok: false })
+        return
+    }
+
+    res.send({ ok: true })
+})
+
+export default router
--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,7 @@ import bodyParser from "body-parser"
 import next from "next"
 import mongoose from "mongoose"
 import { MongoError } from "mongodb"
-// import Account, { IAccount } from "./models/Account"
+import route from "./route"
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -21,26 +21,7 @@ app
         server.use(bodyParser.urlencoded({ extended: true }))
         server.use(bodyParser.json())
 
-        server.post('/account/sign_in', (_req: Request, res: Response): void => {
-            /**
-             * TODO:
-             * 1. body check
-             * 2. create db function
-             * 3. select sql
-             * 4. craete session db
-             * 5. make response interface
-             */
-
-            interface LoginAuthResponse {
-                ok: boolean
-            }
-
-            const response: LoginAuthResponse = {
-                ok: false
-            }
-
-            res.send(response)
-        })
+        server.use('/', route)
 
         server.get('*', (req: Request, res: Response): Promise<void> => {
             return handle(req, res)
