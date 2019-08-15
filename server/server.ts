d9ec106a1ac9a17a726ed5ae0a4116255c0e5594
watanabeJunna <junna7897@icloud.com>
2019-08-15 19:06:27 +0900

    feat: session introduction

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express"
 import bodyParser from "body-parser"
+import session from "express-session"
 import next from "next"
 import mongoose from "mongoose"
 import { MongoError } from "mongodb"
@@ -12,14 +13,31 @@ const handle = app.getRequestHandler()
 app
     .prepare()
     .then(() => {
-        const server = express()
-
         mongoose.connect('mongodb://db:27017/auto_operator', {
             useNewUrlParser: true
         }, (err: MongoError) => { if (err) throw err })
 
+        const server = express()
+
+        let sessionOption = {
+            secret: process.env.SESSION_SIGNNATURE as string,
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+                secure: false,
+                httpOnly: true,
+            }
+        }
+
         server.use(bodyParser.urlencoded({ extended: true }))
         server.use(bodyParser.json())
+
+        if (server.get('env') === 'production') {
+            server.set('trust proxy', 1)
+            sessionOption.cookie.secure = true
+        }
+
+        server.use(session(sessionOption))
 
         server.use('/', route)
 
